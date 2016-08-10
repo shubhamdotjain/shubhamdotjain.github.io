8a7023137d79052cfcbe32fc1852e15705e749af
shubhamjain74 <shubhamjain7495@gmail.com>
2016-08-11 00:31:54 +0530

    added tic tac toe

---
+++ main.js
@@ -0,0 +1,223 @@
+//  window.onload=function()
+//  {
+//  	window.alert('a');
+//  };
+// function add(a,b){
+// 	return a+b;
+// }
+
+// console.log(add(3,2));
+
+// mod_number = function(a)
+// {
+// 	if (a<0) {
+// 		return -1*a;
+// 	}
+// 	else {
+// 		return a;
+// 	}
+// };
+
+// console.log(mod_number(-2));
+window.onload=function()
+{
+	// var a= document.getElementById("one");
+	// a.innerHTML +="Hello world"
+	// // document.getElementById("one").innerHTML +="Hello"
+	// a.style.backgroundColor="Red";
+	// document.getElementById('inc').onclick=function(){
+	// 	var val= document.getElementById('one');
+	// 	var val2= document.getElementById('two');
+	// 	var val3= document.getElementById("answer");
+	// 	 val3.value= Number(val.value)+Number(val2.value); //Parse int
+	//};
+			
+
+		var itr=0;
+
+		document.getElementById("cell1").onclick=function(){
+			var a=document.getElementById("cell1");
+
+			check(itr,a,0)
+			itr++
+			// a.style.width= "30%"
+			// a.style.background="Red"
+			// console.log(itr)
+			// check(itr,a)
+		};
+		document.getElementById("cell2").onclick=function(){
+			var a=document.getElementById("cell2");
+			check(itr,a,1)
+			itr++
+		};
+		document.getElementById("cell3").onclick=function(){
+			var a=document.getElementById("cell3");
+			check(itr,a,2)
+			itr++
+		};
+
+		document.getElementById("cell4").onclick=function(){
+			var a=document.getElementById("cell4");
+			check(itr,a,3)
+			itr++
+		};
+		document.getElementById("cell5").onclick=function(){
+			var a=document.getElementById("cell5");
+			check(itr,a,4)
+			itr++
+		};
+		document.getElementById("cell6").onclick=function(){
+			var a=document.getElementById("cell6");
+			check(itr,a,5)
+			itr++
+		};
+		
+		document.getElementById("cell7").onclick=function(){
+			var a=document.getElementById("cell7");
+			check(itr,a,6)
+			itr++
+		};
+		document.getElementById("cell8").onclick=function(){
+			var a=document.getElementById("cell8");
+			check(itr,a,7)
+			itr++
+		};
+		document.getElementById("cell9").onclick=function(){
+			var a=document.getElementById("cell9");
+			check(itr,a,8)
+			itr++
+		};
+
+
+	
+
+	function check(itr,a,index)
+	{
+		if (itr%2==1)
+		{
+			player2(a,index);
+
+		}
+
+		else {
+			player1(a,index);
+			// console.log(itr)
+		}
+
+		
+		
+	};
+	var arr=[0,0,0,0,0,0,0,0,0]
+
+	function player1(a,index){
+		a.style.width= "30%"
+			a.style.height="30%"
+			a.style.background= "Red";
+			// a.borderRadius= "50%"; 
+			a.style.border="30px solid black";
+			a.setAttribute("id","done");
+			document.getElementById("ttt").style.background="Red"
+			arr [index]=1
+			win()
+		};
+
+	function player2(a,index){
+		a.style.width= "30%"
+			a.style.height="30%"
+			a.style.background= "white";
+			// a.borderRadius= "50%"; 
+			a.style.border="30px solid black";
+			a.setAttribute("id","done");
+			document.getElementById("ttt").style.background="white"
+			arr[index]=2;
+			win()
+};
+	
+	
+
+
+function win()
+{
+			var answer;
+			if(arr[0]==arr[1]&&arr[1]==arr[2])
+			{
+				if (arr[0]==1)
+				{
+					answer= "player 1"
+				}
+				if (arr[0]==2) answer="player 2"
+			}
+			if(arr[3]==arr[4]&&arr[4]==arr[5])
+			{
+				if (arr[3]==1)
+				{
+					answer="player 1"
+				}
+				if (arr[0]==2) answer="player 2"
+			}
+			if(arr[6]==arr[7]&&arr[7]==arr[8])
+			{
+				if (arr[6]==1)
+				{
+					answer="player 1"
+				}
+				if (arr[0]==2) answer="player 2"
+			}
+			if(arr[0]==arr[3]&&arr[3]==arr[6])
+			{
+				if (arr[0]==1)
+				{
+					answer="player 1"
+				}
+				if (arr[0]==2) answer="player 2"
+			}
+			if(arr[1]==arr[4]&&arr[4]==arr[7])
+			{
+				if (arr[1]==1)
+				{
+					answer="player 1"
+				}
+				if (arr[0]==2) answer= "player 2"
+			}
+			if(arr[2]==arr[5]&&arr[5]==arr[8])
+			{
+				if (arr[2]==1)
+				{
+					answer="player 1"
+				}
+				if (arr[0]==2) answer="player 2"
+			}
+
+			if(arr[0]==arr[4]&&arr[4]==arr[8])
+			{
+				if (arr[0]==1)
+				{
+					answer="player 1"
+				}
+				if (arr[0]==2) answer= "player 2"
+			}
+
+			if(arr[6]==arr[4]&&arr[4]==arr[2])
+			{
+				if (arr[6]==1)
+				{
+					answer="player 1"
+				}
+				if (arr[0]==2) answer="player 2"
+			}
+		if (answer=="player 1")
+		{	
+
+			window.alert("Player 1 won")
+		}
+		if(answer== "player 2") window.alert("Player 2 won")
+
+		if(itr==8 && answer!="player 1" && answer!="player 2") window.alert("Draw")
+
+		
+};
+
+function restart()
+{}
+
+};

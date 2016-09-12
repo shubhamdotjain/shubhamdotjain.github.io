34567dc720644f69cf6ba57139c827c0952b41a6
shubhamjain74 <shubhamjain7495@gmail.com>
2016-09-12 16:09:05 +0530

    Restructed the whole thing

--- assignment1/main.py
+++
@@ -1,9 +0,0 @@
-def main():
-	myfile = open("experiments.txt",'wb')
-	# print myfile.name
-	# print dir(myfile)
-	for i in range(100):
-		myfile.write(str(i)+'\n')
-
-if __name__ == '__main__':
-	main()

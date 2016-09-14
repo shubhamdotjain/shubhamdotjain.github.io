82f54ac9af69d8493bccc443f239d29a979c182a
shubhamjain74 <shubhamjain7495@gmail.com>
2016-09-14 17:05:07 +0530

    corrected js file

--- assignment1/main.js
+++ assignment1/main.js
@@ -147,7 +147,7 @@ function win()
 				{
 					answer= "player 1"
 				}
-				if (arr[0]==2) answer="player 2"
+				if (arr[0]==2) {answer="player 2"}
 			}
 			if(arr[3]==arr[4]&&arr[4]==arr[5])
 			{
@@ -155,7 +155,7 @@ function win()
 				{
 					answer="player 1"
 				}
-				if (arr[0]==2) answer="player 2"
+				if (arr[0]==2) {answer="player 2"}
 			}
 			if(arr[6]==arr[7]&&arr[7]==arr[8])
 			{
@@ -163,7 +163,7 @@ function win()
 				{
 					answer="player 1"
 				}
-				if (arr[0]==2) answer="player 2"
+				if (arr[0]==2){ answer="player 2"}
 			}
 			if(arr[0]==arr[3]&&arr[3]==arr[6])
 			{
@@ -171,7 +171,7 @@ function win()
 				{
 					answer="player 1"
 				}
-				if (arr[0]==2) answer="player 2"
+				if (arr[0]==2) {answer="player 2"}
 			}
 			if(arr[1]==arr[4]&&arr[4]==arr[7])
 			{
@@ -179,7 +179,7 @@ function win()
 				{
 					answer="player 1"
 				}
-				if (arr[0]==2) answer= "player 2"
+				if (arr[0]==2) {answer= "player 2"}
 			}
 			if(arr[2]==arr[5]&&arr[5]==arr[8])
 			{
@@ -187,7 +187,7 @@ function win()
 				{
 					answer="player 1"
 				}
-				if (arr[0]==2) answer="player 2"
+				if (arr[0]==2){ answer="player 2"}
 			}
 
 			if(arr[0]==arr[4]&&arr[4]==arr[8])
@@ -196,7 +196,7 @@ function win()
 				{
 					answer="player 1"
 				}
-				if (arr[0]==2) answer= "player 2"
+				if (arr[0]==2){ answer= "player 2"}
 			}
 
 			if(arr[6]==arr[4]&&arr[4]==arr[2])
@@ -205,7 +205,7 @@ function win()
 				{
 					answer="player 1"
 				}
-				if (arr[0]==2) answer="player 2"
+				if (arr[0]==2) {answer="player 2"}
 			}
 		if (answer=="player 1")
 		{	
